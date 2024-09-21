2a1117f3b69ad11fe4c6f370091f3dd31c61a085
YomEngine <120723438+yordgenome03@users.noreply.github.com>
2024-09-22 05:57:08 +0900

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,10 @@
+import 'package:expence_tracker/widgets/expenses.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(
+    const MaterialApp(
+      home: Expenses(),
+    ),
+  );
+}
---
+++ lib/widgets/expense_list/expenses_list.dart
@@ -0,0 +1,20 @@
+import 'package:expence_tracker/models/expense.dart';
+import 'package:expence_tracker/widgets/expense_list/expense_item.dart';
+import 'package:flutter/material.dart';
+
+class ExpensesList extends StatelessWidget {
+  const ExpensesList({
+    super.key,
+    required this.expenses,
+  });
+
+  final List<Expense> expenses;
+
+  @override
+  Widget build(BuildContext context) {
+    return ListView.builder(
+      itemCount: expenses.length,
+      itemBuilder: (ctx, index) => ExpenseItem(expenses[index]),
+    );
+  }
+}
