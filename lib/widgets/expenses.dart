b9ec1f5a80b5991d41ee78ec6432102b9e3bcd7b
YomEngine <120723438+yordgenome03@users.noreply.github.com>
2024-09-23 16:35:57 +0900

    implement to register new expense

--- lib/widgets/expenses.dart
+++ lib/widgets/expenses.dart
@@ -28,9 +28,16 @@ class _ExpensesState extends State<Expenses> {
 
   void _openAddExpenseOverlay() {
     showModalBottomSheet(
+      isScrollControlled: true,
       context: context,
-      builder: (ctx) => const NewExpense(),
+      builder: (ctx) => NewExpense(onAddExpense: _addExpense),
     );
+  }
+
+  void _addExpense(Expense expens) {
+    setState(() {
+      _registeredExpenses.add(expens);
+    });
   }
 
   @override
--- lib/widgets/new_expense.dart
+++ lib/widgets/new_expense.dart
@@ -2,7 +2,9 @@ import 'package:expence_tracker/models/expense.dart';
 import 'package:flutter/material.dart';
 
 class NewExpense extends StatefulWidget {
-  const NewExpense({super.key});
+  const NewExpense({super.key, required this.onAddExpense});
+
+  final void Function(Expense) onAddExpense;
 
   @override
   State<StatefulWidget> createState() => _NewExpenseState();
@@ -52,6 +54,14 @@ class _NewExpenseState extends State<NewExpense> {
       );
       return;
     }
+
+    widget.onAddExpense(Expense(
+      title: _titleController.text,
+      amount: enteredAmount,
+      date: _selectedDate!,
+      category: _selectedCategory,
+    ));
+    Navigator.pop(context);
   }
 
   @override
@@ -69,7 +79,7 @@ class _NewExpenseState extends State<NewExpense> {
   @override
   Widget build(BuildContext context) {
     return Padding(
-      padding: const EdgeInsets.all(16),
+      padding: const EdgeInsets.fromLTRB(16, 48, 16, 16),
       child: Column(
         children: [
           TextField(
