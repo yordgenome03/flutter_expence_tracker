9c86856d87bd83b75c94d813bed5ae020bcf22b7
YomEngine <120723438+yordgenome03@users.noreply.github.com>
2024-09-24 06:46:13 +0900

    add darkmode settings

--- lib/main.dart
+++ lib/main.dart
@@ -5,11 +5,38 @@ var kColorScheme = ColorScheme.fromSeed(
   seedColor: const Color.fromARGB(255, 96, 59, 181),
 );
 
+var kDarkColorScheme = ColorScheme.fromSeed(
+  brightness: Brightness.dark,
+  seedColor: const Color.fromARGB(255, 5, 99, 125),
+);
+
 void main() {
   runApp(
     MaterialApp(
+      darkTheme: ThemeData.dark().copyWith(
+        colorScheme: kDarkColorScheme,
+        cardTheme: const CardTheme().copyWith(
+          color: kDarkColorScheme.secondaryContainer,
+          surfaceTintColor: kDarkColorScheme.onSecondaryContainer,
+          margin: const EdgeInsets.symmetric(horizontal: 16, vertical: 8),
+        ),
+        appBarTheme: const AppBarTheme().copyWith(
+          backgroundColor: kDarkColorScheme.onPrimaryContainer,
+          foregroundColor: kDarkColorScheme.primaryContainer,
+        ),
+        elevatedButtonTheme: ElevatedButtonThemeData(
+          style: ElevatedButton.styleFrom(
+            backgroundColor: kDarkColorScheme.primaryContainer,
+          ),
+        ),
+        textTheme: ThemeData().textTheme.copyWith(
+              titleLarge: TextStyle(
+                  fontWeight: FontWeight.bold,
+                  color: kDarkColorScheme.onSecondaryContainer,
+                  fontSize: 16),
+            ),
+      ),
       theme: ThemeData().copyWith(
-        useMaterial3: true,
         colorScheme: kColorScheme,
         scaffoldBackgroundColor: kColorScheme.secondaryContainer,
         cardTheme: const CardTheme().copyWith(
@@ -28,11 +55,12 @@ void main() {
         ),
         textTheme: ThemeData().textTheme.copyWith(
               titleLarge: TextStyle(
-                  fontWeight: FontWeight.normal,
+                  fontWeight: FontWeight.bold,
                   color: kColorScheme.onSecondaryContainer,
-                  fontSize: 14),
+                  fontSize: 16),
             ),
       ),
+      // themeMode: ThemeMode.system,
       home: const Expenses(),
     ),
   );
--- lib/widgets/expense_list/expenses_list.dart
+++ lib/widgets/expense_list/expenses_list.dart
@@ -18,6 +18,12 @@ class ExpensesList extends StatelessWidget {
       itemCount: expenses.length,
       itemBuilder: (ctx, index) => Dismissible(
         key: ValueKey(expenses[index]),
+        background: Container(
+          color: Theme.of(context).colorScheme.error,
+          margin: EdgeInsets.symmetric(
+            horizontal: Theme.of(context).cardTheme.margin!.horizontal,
+          ),
+        ),
         onDismissed: (_) => onRemoveExpense(expenses[index]),
         child: ExpenseItem(expenses[index]),
       ),
